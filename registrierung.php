2346e4e0d0a27e1ce72d39a3660060c2afd74de6
Felix <if23b032@technikum-wien.at>
2024-06-03 19:26:52 +0200

    changed qodana specs

--- registrierung.php
+++ registrierung.php
@@ -72,10 +72,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 
 function test_input($data)
 {
-    $data = trim($data);
-    $data = stripslashes($data);
-    $data = htmlspecialchars($data);
-    return $data;
+    return htmlspecialchars(stripslashes(trim($data)));
 }
 ?>
 <!DOCTYPE html>
